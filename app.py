97f1c1d5c49d586d60a17119b75352d29002669c
anikarretail <anikar.retail@gmail.com>
2025-05-10 20:08:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def seo_editor_app(label, df, key):
     end_idx = start_idx + batch_size
 
     df['seo_done'] = df['seo_done'].astype(bool)
+        df['seo_done'] = df['seo_done'] & df['desc (product.metafields.custom.desc)'].notnull()
     editable_df = df[(df['desc (product.metafields.custom.desc)'].notnull()) & (~df['seo_done'])].reset_index()
 
     if end_idx > len(editable_df):
@@ -73,7 +74,8 @@ def seo_editor_app(label, df, key):
                 df.at[original_index, 'desc (product.metafields.custom.desc)'] = new_text
                 df.at[original_index, 'SEO Description'] = new_text
                 df.at[original_index, 'Body (HTML)'] = new_text
-                df.at[original_index, 'seo_done'] = True
+                                    if new_text:
+                        df.at[original_index, 'seo_done'] = True
 
         save_data(df, key)
         st.session_state['start_idx'] = end_idx if end_idx < len(editable_df) else 0
